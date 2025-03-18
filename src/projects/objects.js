e226d044b125bc51402ecac16aa184c872cd3327
gabriel <161245822+krsnacg@users.noreply.github.com>
2025-03-18 18:55:54 -0500

    Add class based initialization of json data

--- src/projects/objects.js
+++ src/projects/objects.js
@@ -1,7 +1,5 @@
 import projectsJSON from "./../assets/data/project-data.json" assert {type: "json"}
 
-export const projectData = projectsJSON.projects;
-
 export class Todo {
 
     #creationDate;
@@ -30,8 +28,18 @@ export class Todo {
         if (values.includes(value)) {
             this.#priority = value;
         } else {
-            throw new Error(`Valor no permitido. Debe ser uno de: ${valoresPermitidos.join(', ')}`);
+            throw new Error(`Valor no permitido. Debe ser uno de: ${values.join(', ')}`);
         }
+    }
+
+    toString() {
+        return `Name: ${this.title}, 
+                UUID: ${this.UUID}, 
+                Description: ${this.desc}, 
+                Creation Date: ${this.#creationDate}, 
+                Due Date: ${this.dueDate},
+                Priority: ${this.priority},
+                State: ${this.state}\n`;
     }
 
 }
@@ -44,5 +52,45 @@ export class Project {
         this.todos = todos;
         this.tags = tags
     }
+
+    addTask(task) {
+        if (!(task instanceof Todo)) {
+            throw new TypeError("This element is not an instance of class Todo");
+        }
+        this.todos.push(task);
+    }
+
+    removeTask(index) {
+        if (index > this.getTaskCount() - 1 || index < 0) {
+            console.error(`Invalid task index: ${index}`);
+            return false;
+        }
+        if (this.getTaskCount() > 0) {
+            const removedTask = this.todos.splice(index, 1);
+            console.log(`Removed element at index ${index}: ${removedTask.toString()}`);
+            return true;
+        }
+    }
+
+    toString() {
+        return `Name: ${this.name}\nUUID: ${this.UUID}\nTasks: ${this.todos}`
+    }
+
+    getTaskCount() {
+        return this.todos.length;
+    }
 }
 
+export const projectData = projectsJSON.projects.map((project) => {
+    const todos = project.todos.map((task) => {
+        return new Todo(
+            task.title, 
+            task.desc, 
+            task.creationDate, 
+            task.dueDate, 
+            task.priority.toLocaleUpperCase(), 
+            task.state
+        );
+    });
+    return new Project(project.name, todos);
+});
