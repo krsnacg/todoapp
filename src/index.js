ba28f2805e9ef5989ea578937b362a55d3e41285
gabriel <161245822+krsnacg@users.noreply.github.com>
2025-03-18 19:00:16 -0500

    Remove function calls from initial page load

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import "./styles.css"
-import { displayHome } from "./home"
-import { loadAboutContent } from "./about";
-import { loadMenuContent } from "./menu.js";
+import "./projects/projects.css"
+import { displayProjects } from "./projects/uiloader";
 
-window.addEventListener("load", displayHome);
-
-const buttons = Array.from(document.querySelectorAll("button"));
-
-buttons.at(0).addEventListener("click", () => {
-    displayHome();
+window.addEventListener("load", () => {
+    displayProjects();
 });
-
-buttons.at(1).addEventListener("click", () => {
-    loadMenuContent();
-})
-
-buttons.at(2).addEventListener("click", () => {
-    loadAboutContent();
-})
-
-console.log("UOOH")
-
